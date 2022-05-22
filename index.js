20d7155744c54d0f84993c495de560d139551043
taslima63 <ruba6315@gmail.com>
2022-05-22 16:53:25 +0900

    db coonected

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = "mongodb+srv://carUserAdmin:<password>@cluster0.idcgr.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from carZone!')
+})
+
+app.listen(port, () => {
+    console.log(`carzone app listening on port ${port}`)
+})
