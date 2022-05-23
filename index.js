f8c67dea75e148e4c0684445d476b4ccd74b58a2
taslima63 <ruba6315@gmail.com>
2022-05-23 17:44:22 +0900

    added product api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,12 +18,52 @@ async function run() {
     try {
         await client.connect();
         const partsCollection = client.db('car-zone').collection('carParts');
+        const orderCollection = client.db('car-zone').collection('orders');
+        const userCollection = client.db('car-zone').collection('users');
+
 
         app.get('/carParts', async (req, res) => {
             const query = {};
             const cursor = partsCollection.find(query)
             const parts = await cursor.toArray();
             res.send(parts);
+        });
+
+        app.post('/carParts', async (req, res) => {
+            const carParts = req.body;
+            const result = await partsCollection.insertOne(carParts);
+            res.send(result);
+        });
+
+        app.get('/available/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            const parts = await partsCollection.find(query).toArray();
+            const orders = await orderCollection.find().toArray();
+            parts.forEach(part => {
+                const partsOrdered = orders.filter(order => order.name === part.name);
+                const availableQuantity = parseInt(part.available);
+                const orderedQuantity = parseInt(partsOrdered.order_quantity);
+                console.log("orderedQuantity", orderedQuantity);
+                console.log("availableQuantity", availableQuantity);
+                const available = availableQuantity - partsOrdered?.quantity;
+                parts.available = parseInt(available);
+            });
+            res.send(parts);
+        })
+
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const query = { name: order.name, unit_price: order.unit_price, order_quantity: order.order_quantity, userId: order.userId, user: order.user, userName: order.userName };
+            const exists = await orderCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, order: exists })
+            }
+            const result = await orderCollection.insertOne(order);
+            console.log('sending email');
+            // sendAppointmentEmail(booking);
+            return res.send({ success: true, result });
         });
 
 
